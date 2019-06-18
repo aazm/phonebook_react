ae842746087f20a29fefe4b40e848caa37edb2a1
Alexey Borovkov <lucky.aaz@gmail.com>
2019-06-18 18:56:49 +0300

    file meta progress

--- src/components/BookMeta.js
+++ src/components/BookMeta.js
@@ -3,6 +3,7 @@ import {Card, CardText, CardBody, CardTitle, CardSubtitle, Spinner} from 'reacts
 import config from '../config'
 import axios from 'axios';
 import './BookMeta.css';
+import Moment from 'react-moment';
 
 export default class BookMeta extends Component {
     constructor(props) {
@@ -16,32 +17,28 @@ export default class BookMeta extends Component {
     async componentDidMount() {
 
         const { data } = await axios.get(config.api + '/meta');
-
         this.setState({meta: data, loaded: true});
-
-        console.log('+++');
-        console.log(data);
-        console.log('+++');
     }
 
     render() {
 
         const loaded = this.state.loaded;
+
         let body;
 
         if (loaded) {
 
             const {records_count, page_max_size, filename, file_size, updated_at} = this.state.meta;
-            const link = config.host + '/' + filename;
+            const link = config.host + '/storage/' + filename;
 
             body = <CardBody>
                 <CardTitle>Book Meta Information</CardTitle>
                     <ul className="list-group list-group-flush">
                         <li className="list-group-item">Records count: { records_count }</li>
                         <li className="list-group-item">Page max size: { page_max_size }</li>
-                        <li className="list-group-item">Updated at: { updated_at }</li>
+                        <li className="list-group-item">Updated at:  <Moment format="YYYY-MM-DD HH:MM:SS">{ updated_at }</Moment></li>
                         <li className="list-group-item">File size: { file_size }</li>
-                        <li className="list-group-item"><a href={link} target="blank">File</a></li>
+                        <li className="list-group-item">Download: <a href={link} target="blank">{ filename }</a></li>
                     </ul>
 
             </CardBody>
@@ -50,21 +47,5 @@ export default class BookMeta extends Component {
         }
 
         return <Card>{ body }</Card>
-
-        return (
-            <div>
-                {loaded ?
-                    (<Card>
-                        <CardBody>
-                            <CardTitle>Book Meta Information</CardTitle>
-                            <CardSubtitle>Card subtitle</CardSubtitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the
-                                card's content.</CardText>
-                        </CardBody>
-                    </Card>) : (<span>Loading</span>)
-                }
-
-            </div>
-        )
     }
 }
