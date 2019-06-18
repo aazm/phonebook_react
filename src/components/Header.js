090078809936932ee0acc67a93fc7bff583d99af
Alexey Borovkov <lucky.aaz@gmail.com>
2019-06-18 12:50:18 +0300

    header progress

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+
+export default function Header() {
+    return <header>
+        <div class="d-flex justify-content-center align-items-center">
+            <h3>Phonebook</h3>
+        </div>
+    </header>
+}
