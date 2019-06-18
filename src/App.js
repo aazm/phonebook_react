205ee70490a5b8a020dfa7dc4521753b9fef60fe
Alexey Borovkov <lucky.aaz@gmail.com>
2019-06-18 17:49:12 +0300

    progress with meta

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import PhoneBook from './components/PhoneBook';
+
 function App() {
   return (
     <div className="App">
--- src/components/BookMeta.js
+++ src/components/BookMeta.js
@@ -1,18 +1,52 @@
-import React, { Component } from 'react';
-import { Card, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+import React, {Component} from 'react';
+import {Card, CardText, CardBody, CardTitle, CardSubtitle, Spinner} from 'reactstrap';
+import config from '../config'
+import 'axios';
+import './BookMeta.css';
 
 export default class BookMeta extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            meta: {},
+            loaded: false
+        };
+    }
+
     render() {
+
+
+        const loaded = this.state.loaded;
+        let body;
+
+        if (loaded) {
+            body = <CardBody>
+                <CardTitle>Book Meta Information</CardTitle>
+            </CardBody>
+        } else {
+            body = <CardBody className="d-flex justify-content-center align-items-center"><CardText><Spinner color="primary"/></CardText></CardBody>;
+        }
+
+        return <Card>{ body }</Card>
+
         return (
             <div>
-                <Card>
-                    <CardBody>
-                        <CardTitle>Book Meta Information</CardTitle>
-                        <CardSubtitle>Card subtitle</CardSubtitle>
-                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                    </CardBody>
-                </Card>
+                {loaded ?
+                    (<Card>
+                        <CardBody>
+                            <CardTitle>Book Meta Information</CardTitle>
+                            <CardSubtitle>Card subtitle</CardSubtitle>
+                            <CardText>Some quick example text to build on the card title and make up the bulk of the
+                                card's content.</CardText>
+                        </CardBody>
+                    </Card>) : (<span>Loading</span>)
+                }
+
             </div>
         )
+    }
+
+    componentDidMount() {
+
     }
 }
